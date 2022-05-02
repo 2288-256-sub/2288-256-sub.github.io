6d64b0929afb437d19daa1de4e2c74a815c80b6b
2288-256-sub <101009861+2288-256-sub@users.noreply.github.com>
2022-05-02 14:40:02 +0900

    Add files via upload

---
+++ updateReadme.py
@@ -0,0 +1,20 @@
+import textwrap
+import datetime
+
+t_delta = datetime.timedelta(hours=9)
+JST = datetime.timezone(t_delta, 'JST')
+now = datetime.datetime.now(JST)
+date = now.strftime('%Y/%m/%d %I:%M(%p)')
+
+docs_str = textwrap.dedent('''
+[![Domain Renewal](https://github.com/2288-256-sub/2288-256-sub.github.io/actions/workflows/main.yml/badge.svg)](https://github.com/2288-256-sub/2288-256-sub.github.io/actions/workflows/main.yml)
+
+## 概要
+TwitterのURL欄に表示しているURLのリポジトリ
+
+## IP最終更新日
+{da}
+''').format(da=date).strip()
+
+with open('README.md', 'w') as f:
+    f.write(docs_str)
